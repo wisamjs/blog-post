0020675e562847e33f7cfd965ab948fdd8df653f
wzaghal <wisamzaghal@gmail.com>
2015-02-28 14:03:39 -0500

    add gulp inject

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var watch = require('gulp-watch');
+var inject = require('gulp-inject');
 var appFiles = ['client/**/*.css','client/**/*.js'];
+var jsFiles = appFiles[1];
 
 
 gulp.task('devServer', function() {
@@ -12,9 +14,45 @@ gulp.task('devServer', function() {
   });
 });
 
+function inject(){
+  console.log('INJEEECTING');
+ var target = gulp.src('client/index.html');
+ var sources = gulp.src([jsFiles], {
+  //dont need to read the files, just get the path
+  read: false 
+  });
+
+ return target.pipe(inject(sources, {
+   addRootSlash: false,
+   ignorePath: 'client'
+   //so that path doesn't start with 'client/..'
+  }))
+ .pipe(gulp.dest('client'));
+};
+
 gulp.task('watch', function() {
     watch(appFiles).pipe(connect.reload());
 
+
+    watch(jsFiles,{events:['add','unlink']},function() {
+      gulp.start(['inject']);
+    });
+
 });
 
-gulp.task('default',['devServer','watch']);
+gulp.task('inject', function() {
+ var target = gulp.src('client/index.html');
+ var sources = gulp.src([jsFiles], {
+  //dont need to read the files, just get the path
+  read: false 
+  });
+
+ return target.pipe(inject(sources, {
+   addRootSlash: false,
+   ignorePath: 'client'
+   //so that path doesn't start with 'client/..'
+  }))
+ .pipe(gulp.dest('client'));
+});
+
+gulp.task('default',['inject','devServer','watch']);
